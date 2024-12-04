27ec6c67e08ffba7e7f6774bc63504b44ef294fa
B-O-T-L-Y <agnoter@gmail.com>
2024-12-04 18:14:04 +0200

    feat: docker-entrypoint.sh for backend

---
+++ backend/docker-entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+set -e
+
+echo "Composer install..."
+composer install
+
+file=.env
+
+if [ ! -f "$file" ] ; then
+  cp .env.example .env
+fi
+
+eval $(grep "^APP_KEY=" $file)
+
+if [ -z "$APP_KEY" ] ; then
+  php artisan key:generate
+fi
+
+exec "$@"
+#php artisan serve --host=0.0.0.0
