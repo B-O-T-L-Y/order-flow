c3023035035cddcef146ccdf7bfe02f6723f3f5c
B-O-T-L-Y <agnoter@gmail.com>
2024-12-05 15:46:11 +0200

    feat: make migrations and models and factories and seeders

---
+++ backend/database/factories/OrderFactory.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Order;
+use App\Models\Product;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Order>
+ */
+class OrderFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'user_id' => User::factory(),
+            'status' => 'new',
+        ];
+    }
+
+    public function configure()
+    {
+        return $this->afterCreating(function (Order $order) {
+            $products = Product::factory()->count(3)->create();
+
+            foreach ($products as $product) {
+                $amount = rand(1, 5);
+
+                $order->products()->attach($product, [
+                    'product_name' => $product->name,
+                    'price' => $product->price,
+                    'amount' => $amount,
+                    'total_price' => $product->price * $amount,
+                ]);
+            }
+        });
+    }
+}
