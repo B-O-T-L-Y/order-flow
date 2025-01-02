5e4fd53b86426c7f530074ce44ac566e54558d09
B-O-T-L-Y <agnoter@gmail.com>
2025-01-02 20:48:10 +0200

    feat: add pagination to products and refactor code

--- frontend/src/components/products/ProductsGallery.vue
+++ frontend/src/components/products/ProductsGallery.vue
@@ -1,34 +1,30 @@
 <script setup lang="ts">
 import {onMounted, ref} from "vue";
 import {useCartStore} from "@/stores/useCartStore.ts";
-import {useApiFetch} from "@/composables/useApiFetch.ts";
+import {useProductsStore} from "@/stores/useProductsStore.ts";
 
-const products = ref([]);
+const productsStore = useProductsStore();
 const cartStore = useCartStore();
-const cartProductNmae = ref<string | null>(null);
+const cartProductName = ref<string | null>(null);
 const showSuccess = ref<boolean>(false);
 
-const fetchProducts = async () => {
-  const {data, error} = await useApiFetch('/api/products').json();
-
-  if (!error.value) {
-    products.value = data.value.data;
-  }
+const fetchProducts = async (page: number) => {
+  await productsStore.fetchProducts(page);
 };
 
 const addCart = (product: Product) => {
   cartStore.addToCart(product);
 
-  cartProductNmae.value = product.name;
+  cartProductName.value = product.name;
   showSuccess.value = true;
 
   setTimeout(() => {
-    cartProductNmae.value = null;
+    cartProductName.value = null;
     showSuccess.value = false;
   }, 3000);
 };
 
-onMounted(fetchProducts);
+onMounted(() => fetchProducts(1));
 </script>
 
 <template>
@@ -36,18 +32,18 @@ onMounted(fetchProducts);
     <div
       v-if="showSuccess"
       class="fixed top-4 right-4 bg-green-100 text-green-800 px-4 py-2 rounded-lg shadow-lg">
-      Add <b>{{ cartProductNmae }}</b> to cart.
+      Add <b>{{ cartProductName }}</b> to cart.
     </div>
   </transition>
 
   <div class="grid grid-cols-2 md:grid-cols-6 gap-4">
     <div
-      v-for="product in products"
+      v-for="product in productsStore.products"
       :key="product.id"
       class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
     >
       <img class="p-8 rounded-t-lg" src="https://flowbite.com/docs/images/products/apple-watch.png" alt="Product image"/>
-      <div class="px-5 pb-5">
+      <div class="px-5 pb-5 space-y-4">
         <h5 class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">{{ product.name }}</h5>
         <div class="flex items-center justify-between">
           <span class="text-3xl font-bold text-gray-900 dark:text-white">{{ product.price }}</span>
@@ -61,6 +57,54 @@ onMounted(fetchProducts);
       </div>
     </div>
   </div>
+  <nav
+    v-if="productsStore.pagination.lastPage > 1"
+    aria-label="Page navigation example"
+    class="mt-4"
+  >
+    <ul class="inline-flex -space-x-px text-sm">
+      <li>
+        <button
+          @click="fetchProducts(productsStore.pagination.currentPage - 1)"
+          :disabled="productsStore.pagination.currentPage === 1"
+          aria-label="Previous"
+          class="flex items-center justify-center px-3 h-8 ms-0 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+        >
+          Previous
+        </button>
+      </li>
+
+      <li
+        v-for="page in Array.from({ length: productsStore.pagination.lastPage }, (_, i) => i + 1)"
+        :key="page"
+      >
+        <button
+          @click="fetchProducts(page)"
+          :aria-current="productsStore.pagination.currentPage === page ? 'page' : null"
+          :aria-label="`Page ${page}`"
+          :disabled="productsStore.pagination.currentPage === page"
+          class="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+          :class="{
+          'bg-gray-100 text-gray-700 dark:bg-gray-500 dark:text-white': productsStore.pagination.currentPage === page,
+        }"
+        >
+          {{ page }}
+        </button>
+      </li>
+
+      <li>
+        <button
+          @click="fetchProducts(productsStore.pagination.currentPage + 1)"
+          :disabled="productsStore.pagination.currentPage === productsStore.pagination.lastPage"
+          aria-label="Next"
+          class="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+        >
+          Next
+        </button>
+      </li>
+    </ul>
+  </nav>
+
 </template>
 
 <style scoped>
---
+++ frontend/src/stores/useProductsStore.ts
@@ -0,0 +1,30 @@
+import {defineStore} from "pinia";
+import {useApiFetch} from "@/composables/useApiFetch.ts";
+import {ref} from "vue";
+
+export const useProductsStore = defineStore('products', () => {
+  const products = ref([]);
+  const pagination = ref({
+    currentPage: 1,
+    lastPage: 1,
+  });
+
+  const fetchProducts = async (page: number = 1): Promise<void> => {
+    const endpoint = `/api/products?page=${page}`;
+    const {data, error, statusCode} = await useApiFetch(endpoint).json();
+
+    if (statusCode.value === 200) {
+      products.value = data.value.data;
+      pagination.value = {
+        currentPage: data.value.current_page,
+        lastPage: data.value.last_page,
+      };
+    }
+  };
+
+  return {
+    products,
+    pagination,
+    fetchProducts,
+  };
+});
